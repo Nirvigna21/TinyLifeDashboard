299d931b355e7a2227fa83bf397c5ff4276f88cf
Nirvigna21 <nirvignyaa05@gmail.com>
2025-10-16 15:15:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Load saved data on page load
+
 window.onload = function() {
     if(localStorage.getItem('water')) document.getElementById('waterDisplay').innerText = localStorage.getItem('water') + " glasses";
     if(localStorage.getItem('steps')) document.getElementById('stepsDisplay').innerText = localStorage.getItem('steps') + " steps";
     if(localStorage.getItem('mood')) document.getElementById('moodDisplay').innerText = localStorage.getItem('mood');
     if(localStorage.getItem('note')) document.getElementById('noteDisplay').innerText = localStorage.getItem('note');
 }
-
-// Functions to save data
 function saveWater() {
     let water = document.getElementById('waterInput').value;
     localStorage.setItem('water', water);
